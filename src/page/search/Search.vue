d262cc53fa0efbf2f15f8162b11dde4506e4b8fd
lxt <1425351337@qq.com>
2024-04-28 20:54:46 +0800

    搜索页

--- src/page/search/Search.vue
+++ src/page/search/Search.vue
@@ -5,25 +5,69 @@
                 v-model="value"
                 show-action
                 placeholder="请输入搜索关键词"
+                action-text="搜索"
                 @search="onSearch"
                 @cancel="onCancel"
+                @input="onInput"
             />
         </form>
+        <van-cell-group>
+            <van-cell @click="getResultList(item)" v-for="item in suggestList" icon="search" :title="item"/>
+        </van-cell-group>
+        <van-list
+            finished-text="没有更多了"
+            >
+            <van-cell v-for="item in resultList" @click="goPlay(item.id)" :title="item.name" >
+                <template #right-icon>
+                    <van-icon name="play-circle" class="search-icon" />
+                </template>
+            </van-cell>
+        </van-list>
     </div>
 </template>
 
 <script>
+import { searchSuggestApi,searchApi } from '@/base/api';
 
 export default {
+
     data() {
         return {
+            value:'',
+            suggestList:[],
+            resultList:[],
+            timer:null
         }
     },
-
     methods: {
-    
+        onSearch() {
+            this.getResultList()
+        },
+        onCancel() {
+            this.getResultList()
+        },
+        onInput() {
+            if(this.timer)clearTimeout(this.timer)
+            this.timer = setTimeout(async () => {
+                this.resultList=[]
+                this.suggestList=[]
+                if(this.value.length<=0) return
+                const res = await searchSuggestApi(this.value)
+                res.result.songs.forEach(v=>{
+                    this.suggestList.push(v.name)
+                })
+            }, 200);
+        },
+        async getResultList(val=this.value) {
+            this.value=val
+            const res = await searchApi(this.value)
+            this.suggestList=[]
+            this.resultList=res.result.songs
+        },
+        goPlay(id) {
+            console.log(id)
+        }
     },
-
 }
 </script>
 
